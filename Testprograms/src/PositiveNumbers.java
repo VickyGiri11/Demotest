18bccf3ab7ba53f8c7f498a9e3b4f65dc6042aa8
7702 <7702@TNQ-LTP-715.tnq.co.in>
2024-10-26 17:54:46 +0530

    First Push

---
+++ Testprograms/src/PositiveNumbers.java
@@ -0,0 +1,17 @@
+package src;
+
+public class PositiveNumbers 
+{
+public static void main(String [] args)
+    {
+	System.out.println("Program starts");
+	int number = 10;
+	if(number>0) // if mnumber is greated than zero we can consider numbers is positive
+		System.out.println("number is positive : "+number);
+	}
+    else
+    {
+	    System.out.println("number is negative : "+number);
+	}
+    System.out.println("Program end");
+}
---
+++ Testprograms/src/modulus.java
@@ -0,0 +1,23 @@
+
+public class modulus {
+
+	public static void main(String[] args) 
+	{
+		// Arthimeticoperation
+		int n1 = 4;
+		int n2 = 6;
+		int res = n1+n2;
+		int result = n2-n1;
+		int result1 = n2*n1;
+		int result2 = n2/n1;
+		int result3 = n2%n1;
+		System.out.println(res);
+		System.out.println(result);
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		
+		
+	}
+
+}
